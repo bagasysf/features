265939d00ddf6064d4628e1915cc4dbe95764163
Babang <46173577+bagasysf@users.noreply.github.com>
2024-04-09 13:33:44 +0700

    Destructuring with Annotations

--- function.ts
+++ function.ts
@@ -24,3 +24,21 @@ const throwWrro = (message: string): string => {
   }
   return message;
 };
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
