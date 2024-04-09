545e4276ccd89d466b6529ed54031bf6fbb59294
Babang <46173577+bagasysf@users.noreply.github.com>
2024-04-09 18:32:17 +0700

    Field in Classes

--- clasess.ts
+++ clasess.ts
@@ -3,24 +3,34 @@ class Vehicle {
   //   console.log(`chugga chugga`);
   // }
 
+  constructor(public color: string) {
+    this.color = color;
+  }
+
   protected honk(): void {
     console.log('beep');
   }
-}
 
-class Car extends Vehicle {
-  private drive(): void {
-    console.log(`vroom`);
-  }
-
-  startDrivingProcess(): void {
-    this.drive();
-    this.honk();
+  public get getColor(): string {
+    return this.color;
   }
 }
 
-const vehicle = new Vehicle();
-const car = new Car();
-car.startDrivingProcess();
-vehicle.honk();
-car.honk();
+const vehicle = new Vehicle('orange');
+console.log(vehicle.getColor);
+
+// class Car extends Vehicle {
+//   private drive(): void {
+//     console.log(`vroom`);
+//   }
+
+//   startDrivingProcess(): void {
+//     this.drive();
+//     this.honk();
+//   }
+// }
+
+// const car = new Car();
+// car.startDrivingProcess();
+// vehicle.honk();
+// car.honk();
