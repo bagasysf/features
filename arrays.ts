f575f8b887256f695177e797251104f9022e3cf2
Babang <46173577+bagasysf@users.noreply.github.com>
2024-04-09 14:03:38 +0700

    Why type arrays

--- arrays.ts
+++ arrays.ts
@@ -5,3 +5,15 @@ const dates = [new Date(), new Date()];
 
 // const carsByMake: string[][] = [['f150'], ['corolla'], ['camaro']];
 const carsByMake: string[][] = [];
+
+// Help with inferences when extracting values
+const anotherCar = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car;
+});
