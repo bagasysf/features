5b3537f35790f8d0e6a1edf7fa7c39ecacdd6b6f
Babang <46173577+bagasysf@users.noreply.github.com>
2024-04-09 11:48:38 +0700

    Object Literal Annotations

---
+++ variable.ts
@@ -0,0 +1,27 @@
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let notingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objets
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truth: boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
+
+
