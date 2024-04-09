0fb988fe2c6cde183c08c680316b8faa25d7c906
Babang <46173577+bagasysf@users.noreply.github.com>
2024-04-09 16:13:46 +0700

    Code reuse with interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -1,17 +1,28 @@
+interface Reportable {
+  summary(): string;
+}
+
 const oldCivic = {
   name: 'civic',
-  year: 2000,
+  year: new Date(),
   broken: true,
+  summary(): string {
+    return `Name ${this.name}`;
+  },
 };
 
-const printVehicle = (vehicle: {
-  name: string;
-  year: number;
-  broken: boolean;
-}): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken? ${vehicle.broken}`);
+const minuman = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} gram of sugar`;
+  },
 };
 
-printVehicle(oldCivic);
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
+};
+
+printSummary(oldCivic);
+printSummary(minuman);
