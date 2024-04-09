fb383af48b05c007b80308b0381b5df8e69e9e0e
Babang <46173577+bagasysf@users.noreply.github.com>
2024-04-09 13:56:44 +0700

    Arrays in Typescript

---
+++ arrays.ts
@@ -0,0 +1,7 @@
+const carMakers = ['ford', 'toyota', 'chevy'];
+const emptyArray: string[] = [];
+
+const dates = [new Date(), new Date()];
+
+// const carsByMake: string[][] = [['f150'], ['corolla'], ['camaro']];
+const carsByMake: string[][] = [];
